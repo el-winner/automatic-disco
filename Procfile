a28c3bf21a8adadfea2b51ec607b18852428dd59
el-winner <ganievaea15@gmail.com>
2022-05-15 22:25:10 +0300

    Удален Procfile

--- Procfile
+++
@@ -1,1 +0,0 @@
-java -Dserver.port=52529 $JAVA_OPTS -jar build/libs/demo-0.0.1-SNAPSHOT.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -37,4 +37,8 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
+tasks.withType<Jar> {
+	manifest {
+		attributes["Main-Class"] = "com.example.demo.DemoApplicationKt"
+	}
+}
